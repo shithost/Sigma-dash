3e2e795d1051a677feea506adbdfbd872cfa71c8
root <root@test.local>
2025-03-11 12:49:30 +0000

    added proxy and vpn check

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
 const DiscordStrategy = require('passport-discord').Strategy;
+const axios = require('axios');
 
 const app = express();
-
 
 app.use(session({
   secret: process.env.SESSION_SECRET,
@@ -36,6 +36,21 @@ passport.deserializeUser((obj, done) => {
   done(null, obj);
 });
 
+const checkVPN = async (req, res, next) => {
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  try {
+    const response = await axios.get(`http://proxycheck.io/v2/${ip}?key=${process.env.PROXYCHECK_API_KEY}&vpn=1&proxy=1&asn=1&node=1&port=1&seen=1&inf=1&days=180`);
+    const data = response.data[ip];
+    if (data.proxy === 'yes' || data.vpn === 'yes' || data.tor === 'yes') {
+      return res.status(403).send('VPN, Proxy, or Tor is not allowed.');
+    }
+    next();
+  } catch (error) {
+    console.error('Error checking VPN:', error);
+    res.status(500).send('Internal Server Error');
+  }
+};
+
 app.get('/', (req, res) => {
   if (req.isAuthenticated()) {
     res.redirect('/dashboard');
@@ -44,7 +59,7 @@ app.get('/', (req, res) => {
   }
 });
 
-app.get('/auth/discord', passport.authenticate('discord'));
+app.get('/auth/discord', checkVPN, passport.authenticate('discord'));
 
 app.get('/auth/discord/callback', passport.authenticate('discord', { failureRedirect: '/' }),
   (req, res) => {
@@ -66,7 +81,7 @@ app.get('/logout', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
